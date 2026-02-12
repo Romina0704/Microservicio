46b92de987fd4a3132a8c22ee16bba80a179f2eb
juliana <julianapino25@gmail.com>
2026-02-12 13:32:54 -0500

    micro-pedidos

---
+++ src/main/java/com/microservicio/microserviciopedidos/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.microservicio.microserviciopedidos.repository;
+
+import com.microservicio.microserviciopedidos.entidad.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
